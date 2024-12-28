2f1ef12944df56e43d393b6effe45b410d4cb092
Nirupam Pal <nirupampal14@gmail.com>
2024-12-29 00:20:35 +0530

    Added more animations to the landing page

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -6,18 +6,23 @@ tl.from(".animate-nav",{
     y: -10,
     opacity: 0,
     duration: 1,
-    // stagger: 1
 })
 tl.from(".img",{
     x: -70,
     opacity: 0,
     duration: 0.5,
-    // delay: 0.5,
 })
 tl.from(".hamburger",{
     x: 70,
     opacity: 0,
     duration: 0.5,
+})
+tl.from(".brandName",{
+    y:20,
+    opacity: 0,
+    duration: 1,
+    scale: 0.2,
+    color:"black"
 })
 
 //about
@@ -30,20 +35,76 @@ gsap.from(".animate-about-strong",{
         trigger: ".animate-about-strong",
         scroller: "body",
         markers: false,
-        start:"top 100%",
+        start:"top 180%",
         scrub: 2
     }
 })
 gsap.from(".animate-about",{
     opacity:0,
     scale: 0,
-    duration: 0.3,
+    duration: 0.1,
     x: 400,
     scrollTrigger: {
         trigger: ".animate-about",
         scroller: "body",
         markers: false,
+        start:"top 180%",
+        scrub: 2
+    }
+})
+gsap.from(".animate-para",{
+    opacity:0,
+    scale: 0,
+    duration: 0.2,
+    x: -40,
+    scrollTrigger: {
+        trigger: ".animate-para",
+        scroller: "body",
+        markers: false,
+        start:"top 180%",
+        scrub: 2
+    }
+})
+
+//Skill and experience
+
+gsap.from(".big-experience",{
+    opacity:0,
+    scale: 0,
+    duration: 0.1,
+    // x: -40,
+    scrollTrigger: {
+        trigger: ".big-experience",
+        scroller: "body",
+        markers: false,
+        start:"top 190%",
+        scrub: 2
+    }
+})
+gsap.from(".info-content",{
+    opacity:0,
+    // scale: 0,
+    duration: 0.1,
+    x: 100,
+    scrollTrigger: {
+        trigger: ".info-content",
+        scroller: "body",
+        markers: false,
         start:"top 100%",
+        scrub: 2
+    }
+})
+
+tl.from(".skill-box-animate",{
+    opacity: 0,
+    duration: 0.1,
+    y: -40,
+    stagger: 0.3,
+    scrollTrigger:{
+        trigger: ".skill-box-animate",
+        scroller:"body",
+        markers: false,
+        start: "top 100%",
         scrub: 2
     }
 })
